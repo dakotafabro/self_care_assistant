0cfb33cbd53ec89e2a6b801fa161aed2f0a5775f
dakotafabro <dakotafabrocgu@gmail.com>
2022-03-09 12:06:01 -0800

    Added Bored API to site

--- src/index.js
+++ src/index.js
@@ -22,7 +22,6 @@ let supportOption3 = document.querySelector("#work-support");
 supportOption3.addEventListener("click", () => toggleClasses("#work-expand"));
 
 // setting the date, time, and weather
-
 // setting date
 let months = [
   "January",
@@ -57,11 +56,11 @@ time.innerHTML = `${today} - ${hours}:${minutes}`;
 
 // setting weather
 let weather = document.querySelector("#weather");
-weather.innerHTML = `Weather`;
+weather.innerHTML = `64&deg; ICON`;
 
 // setting and cycling through self-care options
 let self_care_options = [
-  "Drink a glass of water",
+  "Drink a glass of water ",
   "Go for a 10-minute walk",
   "Text a friend",
   "Take 5 deep breaths",
@@ -74,18 +73,74 @@ let self_care_options = [
   "Order your favorite food",
   "Get some sunshine",
   "Tell your houseplant positive affirmations",
+  "Take up a new skill--like ceramics!",
+  "Take the next 20 minutes as quiet time",
+  "Schedule a rest day with your partner or someone close to you",
+  "Cuddle with your pet",
+  "Schedule a wellness appointment with your doctor or therapist",
+  "Invite a friend or chosen family member over to spend some time together",
+  "Schedule a massage--schedule monthly massages for bonus points!",
+  "Look up new recipe and try it out!",
+  "Create some art",
+  "Go outside and breathe in some fresh air",
+];
+
+let work_options = [
+  "Drink a glass of water ",
+  "Go for a 10-minute walk",
+  "Text a friend",
+  "Take 5 deep breaths",
+  "Make some tea",
+  "Watch your favorite movie",
+  "Take a shower",
+  "Call a friend",
+  "Journal for 10 minutes",
+  "Go give some loves to your pet!",
+  "Order your favorite food",
+  "Get some sunshine",
+  "Tell your houseplant positive affirmations",
+  "Take up a new skill--like ceramics!",
+  "Take the next 20 minutes as quiet time",
+  "Schedule a rest day with your partner or someone close to you",
+  "Cuddle with your pet",
+  "Schedule a wellness appointment with your doctor or therapist",
+  "Invite a friend or chosen family member over to spend some time together",
+  "Schedule a massage--schedule monthly massages for bonus points!",
+  "Look up new recipe and try it out!",
+  "Create some art",
+  "Go outside and breathe in some fresh air",
 ];
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-function getSelfCareOption(list, element) {
+function getOption(list, element) {
   let result = document.querySelector(element);
   result.innerHTML = list[getRandomInt(list.length)];
 }
 
+function handleResponse(response) {
+  let personalResult = document.querySelector("#personal-result");
+  personalResult.innerHTML = response.data.activity;
+}
+
+function getBoredAPIOption() {
+  const url = "http://www.boredapi.com/api/activity/";
+
+  axios.get(url).then(handleResponse);
+}
+
+// add functionality of iterating through lists/responses for options
 let selfCareButton = document.querySelector("#self-care-button");
 selfCareButton.addEventListener("click", () =>
-  getSelfCareOption(self_care_options, "#self-care-result")
+  getOption(self_care_options, "#self-care-result")
+);
+
+let personalButton = document.querySelector("#personal-button");
+personalButton.addEventListener("click", getBoredAPIOption);
+
+let workButton = document.querySelector("#work-button");
+workButton.addEventListener("click", () =>
+  getOption(work_options, "#work-result")
 );
